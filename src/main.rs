5cf02bf38fb0ea9458ce835c8937971760c8eaad
el-hadji-mamadou-sarr <elhadji.sarr@outlook.com>
2024-03-12 22:50:14 +0100

    list all bluetooth devices

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+use btleplug::api::{Central, Manager as _, Peripheral as _, ScanFilter, WriteType};
+use btleplug::platform::{Adapter, Manager, Peripheral};
+
+use std::error::Error;
+
+use std::time::Duration;
+use tokio::time;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let manager = Manager::new().await.unwrap();
+
+    // get the first bluetooth adapter
+    let adapters = manager.adapters().await?;
+    let central = adapters.into_iter().nth(0).unwrap();
+
+    // start scanning for devices
+    central.start_scan(ScanFilter::default()).await?;
+    // instead of waiting, you can use central.events() to get a stream which will
+    // notify you of new devices, for an example of that see examples/event_driven_discovery.rs
+    time::sleep(Duration::from_secs(2)).await;
+
+    for peripheral in central.peripherals().await? {
+        let properties = peripheral.properties().await?.unwrap();
+        println!("Device Name: {:?}", properties.local_name);
+        println!("Address: {:?}", properties.address);
+        println!("Signal Strength: {:?}", properties.tx_power_level);
+        println!("--------------------------------------------------");
+    }
+    Ok(())
 }
